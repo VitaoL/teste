f5f49c52238b413be1b41555a359882ff2cba0b3
VitaoL <62264826+VitaoL@users.noreply.github.com>
2025-11-27 00:13:41 -0300

    Update database configuration

--- Clave-main/Clave-main/src/persistencia/database.js
+++ Clave-main/Clave-main/src/persistencia/database.js
@@ -1,62 +1,12 @@
-<<<<<<< HEAD
-// src/persistencia/database.js
+require('dotenv').config();
 const { Pool } = require('pg');
 
-class database {
-    static #pool = null;
+const pool = new Pool({
+    user: 'admin',
+    host: '127.0.0.1',
+    database: 'meubanco',
+    password: process.env.DB_PASSWORD, // aqui vamos colocar senha123 no .env
+    port: 5432,
+});
 
-    static getPool() {
-        if (!this.#pool) {
-            this.#pool = new Pool({
-                user: 'postgres',
-                host: 'localhost',
-                database: 'clave_db',
-                password: 'sua_senha', // SUA SENHA AQUI
-                port: 5432,
-            });
-            console.log('Pool de conexões com o PostgreSQL estabelecido!');
-        }
-        return this.#pool;
-    }
-
-    static async closePool() {
-        if (this.#pool) {
-            await this.#pool.end();
-            console.log('Pool de conexões com o banco fechado.');
-            this.#pool = null;
-        }
-    }
-}
-
-=======
-// src/persistencia/database.js
-const { Pool } = require('pg');
-
-class database {
-    static #pool = null;
-
-    static getPool() {
-        if (!this.#pool) {
-            this.#pool = new Pool({
-                user: 'postgres',
-                host: 'localhost',
-                database: 'clave_db',
-                password: 'sua_senha', // SUA SENHA AQUI
-                port: 5432,
-            });
-            console.log('Pool de conexões com o PostgreSQL estabelecido!');
-        }
-        return this.#pool;
-    }
-
-    static async closePool() {
-        if (this.#pool) {
-            await this.#pool.end();
-            console.log('Pool de conexões com o banco fechado.');
-            this.#pool = null;
-        }
-    }
-}
-
->>>>>>> 34ba386cc9e0d9669e4166a584f8edc6bdbf0446
-module.exports = database;
+module.exports = pool;
